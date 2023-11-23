6fb88d82ee8568c115de2347598aae7e0596c9b5
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-23 16:10:36 +0100

    CHANGE - Pw generator

--- index.php
+++ index.php
@@ -14,23 +14,48 @@
         <input type="submit" value="Submit">
     </form>
 </body>
+
 <?php
+
 $pw_length = isset($_GET['pw_length']) ? $_GET['pw_length'] : null;
+
 $pw_source = 'abcdefghijklmnopqrstuvzywxABCDEFGHIJKLMNOPQRSTUVZYWX@#-.:_;!"£$%&/()=?^><';
+
 $source_length = strlen($pw_source);
+
 $char_array = [];
-if ($pw_length) {
-    function pw_generator($pw_length, $source_length, $pw_source, &$char_array)
-    {
-        for ($i = 0; $i < $pw_length; $i++) {
-            $current_char = random_int('0', $source_length);
-            array_push($char_array, $pw_source[$current_char]);
-            var_dump($char_array);
-        }
-        return $char_array;
+
+function pw_generator($pw_length, $source_length, $pw_source, $char_array)
+{
+    for ($i = 0; $i < $pw_length; $i++) {
+        $current_char = random_int('0', $source_length - 1);
+        array_push($char_array, $pw_source[$current_char]);
     }
-    var_dump($char_array);
+    return $char_array;
 }
+
+if ($pw_length != null) {
+    $char_pw = pw_generator($pw_length, $source_length, $pw_source, $char_array);
+    $password = implode($char_pw);
+    var_dump($password);
+}
+
+
+
+// if ($pw_length != null) {
+//     function pw_generator($pw_length, $source_length, $pw_source, &$char_array)
+//     {
+//         for ($i = 0; $i < $pw_length; $i++) {
+//             $current_char = random_int('0', $source_length);
+
+//             var_dump($current_char);
+
+//             array_push($char_array, $pw_source[$current_char]);
+//         }
+//         return $char_array;
+//     }
+//     var_dump($char_array);
+// }
 
 ?>
 
