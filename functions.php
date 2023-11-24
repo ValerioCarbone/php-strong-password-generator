ee77598563319e3c32bcd91ab0b0692f0ef265df
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-24 10:10:30 +0100

    CHANGE - Refactory code

--- functions.php
+++ functions.php
@@ -3,10 +3,10 @@ function pw_generator($pw_length)
 {
     $pw_source = '0123456789abcdefghijklmnopqrstuvzywxABCDEFGHIJKLMNOPQRSTUVZYWX@#-_!$%&/()=?^';
     $password = '';
-    $max_char = strlen($pw_source);
+    $max_char = strlen($pw_source) - 1;
     for ($i = 0; $i < $pw_length; $i++) {
 
-        $current_char = random_int('0', $max_char - 1);
+        $current_char = random_int('0', $max_char);
 
         $password .= $pw_source[$current_char];
     }
