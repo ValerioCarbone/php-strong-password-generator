51734b70cbce537bd5e011b7f25136d6fa11a4b8
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-24 09:55:51 +0100

    CHANGE - Refactory function, strlen out of cicle

--- functions.php
+++ functions.php
@@ -3,9 +3,10 @@ function pw_generator($pw_length)
 {
     $pw_source = '0123456789abcdefghijklmnopqrstuvzywxABCDEFGHIJKLMNOPQRSTUVZYWX@#-_!$%&/()=?^';
     $password = '';
+    $max_char = strlen($pw_source);
     for ($i = 0; $i < $pw_length; $i++) {
 
-        $current_char = random_int('0', strlen($pw_source) - 1);
+        $current_char = random_int('0', $max_char - 1);
 
         $password .= $pw_source[$current_char];
     }
